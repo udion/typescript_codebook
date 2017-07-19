dcf5ee8ecdbc69b4f1bce41720adb6b5484bbae4
udion <uddeshya.upa@gmail.com>
2017-07-19 13:58:34 +0530

    type mismatch example

---
+++ helloWorld/hello-world.js
@@ -0,0 +1,6 @@
+function helloWorld(person) {
+    return "Hi " + person;
+}
+var user1 = "udion";
+var user2 = [0, 1, 2];
+document.body.innerHTML = helloWorld(user2);
---
+++ helloWorld/hello-world.ts
@@ -0,0 +1,7 @@
+function helloWorld(person: String) {
+   return "Hi "+ person;
+}
+
+var user1 = "udion";
+var user2 = [0,1,2];
+document.body.innerHTML = helloWorld(user1);
